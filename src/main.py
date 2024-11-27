022c5458edec598cb0133f3d261ac565df7bcc23
Miha Donzhuk <michael01@bk.ru>
2024-11-27 13:47:28 +0300

    master_Ver_1

--- src/main.py
+++ src/main.py
@@ -1,23 +1,49 @@
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, filters
+from aiogram import Bot, Dispatcher, executor
 import asyncio
-from aiogram.types import Message
 from aiohttp import ClientSession
+import logging
+from aiogram.types import Message
+from src import config
 
 load_dotenv()
 
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
-@dp.message_handler(filters.CommandStart())
+@dp.message_handler(commands=['start'])
 async def cmd_start(message: Message):
-    await message.answer('Привет!')
+    '''Обработчик команды старт'''
+    await message.answer(f'Привет, {message.from_user.first_name}!')
+
+@dp.message_handler()
+async def unknown_cmd(message: Message):
+    '''Обработчик незнакомых команд'''
+    await message.reply('Я не знаю такой команды')
+@dp.message_handler(commands=['market'])
+async def view_market(message: Message):
+    '''Обработчик команды просмотр магазина'''
+    await message.answer('Обзор магазина')
 
 async def main():
-    async with ClientSession() as session:  # Использование менеджера контекста
-        bot["client_session"] = session
+    session = ClientSession() # Использование менеджера контекста для закрытия сессии
+    bot["client_session"] = session
+    try:
         await dp.start_polling(bot)
+    except asyncio.CancelledError:
+        print("Polling stopped")
+    finally:
+        await session.close()  # Закрываем сессию в любом случае
+        await bot.close()  # Закрываем бота
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    if config.DEBUG:
+        logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp)
+
+    # try:
+    #     asyncio.run(main())
+    # except KeyboardInterrupt:
+    #     print('Exit')
